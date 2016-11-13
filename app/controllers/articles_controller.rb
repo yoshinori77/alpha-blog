200104c9a3e631c395589f123262312e2988cc58
yamashitayoshinori <yoshinoriyamashita123@gmail.com>
2016-11-13 08:59:44 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
     end
     
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
@@ -59,5 +59,4 @@ class ArticlesController < ApplicationController
         redirect_to root_path
       end
     end
-  
 end
